1850aaa8013a15f2ca88d08e24d2e78a29b0e1ae
Dar Malovani <dar.malovani@wilcohq.com>
2022-12-19 16:48:08 +0200

    chore: better error messages on the backend test

--- tests/is_verified_backend.js
+++ tests/is_verified_backend.js
@@ -15,23 +15,23 @@ const testItems = async () => {
   const items = await getItems();
   const notVerifiedSellerItem = items.find(i => i.slug === 'not_verified_seller_item');
   if (!notVerifiedSellerItem) {
-    console.log(`=!=!=!=!= ERROR: could not find item for non verified seller`);
+    console.log(`=!=!=!=!= ERROR: could not find item for non verified seller when calling /api/items`);
     return false;
   }
 
   if (notVerifiedSellerItem?.seller?.isVerified === undefined) {
-    console.log(`=!=!=!=!= ERROR: the item's seller doesn't have the "isVerified" field`);
+    console.log(`=!=!=!=!= ERROR: the item's seller doesn't have the "isVerified" field when calling /api/items`);
     return false;
   }
 
   if (notVerifiedSellerItem?.seller?.isVerified !== false) {
-    console.log(`=!=!=!=!= ERROR: "isVerified" is not set to "false" a non verified seller`);
+    console.log(`=!=!=!=!= ERROR: "isVerified" is not set to "false" a non verified seller when calling /api/items`);
     return false;
   }
 
   const verifiedSellerItem = items.find(i => i.slug === 'verified_seller_item');
   if (!verifiedSellerItem) {
-    console.log(`=!=!=!=!= ERROR: could not find item for verified seller`);
+    console.log(`=!=!=!=!= ERROR: could not find item for verified seller when calling /api/items`);
     return false;
   }
 
